15a18406077616091fb8b74ecc199dd89906177d
Charles Kim <cwkim3@gmail.com>
2018-06-28 22:42:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function happyHolidays(){
   return "Happy holidays!"}
 function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`}
-happyHolidaysTo(holiday, name){
+function happyHolidaysTo(holiday, name){
   return `Happy ${holiday}, ${name}!`
   }
